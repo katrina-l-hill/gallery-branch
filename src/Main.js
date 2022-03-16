50a0d4b88414d47c73f507f0fc9b6a3836ed321c
Katrina Lenell Hill <khill365@gmail.com>
2022-03-15 20:23:32 -0700

    added array for animals to pass from json file

--- src/Main.js
+++ src/Main.js
@@ -4,15 +4,29 @@ import React from 'react';
 
 import HornedBeast from './HornedBeast.js';
 
-import data from './data.json';
-console.log(data);
+// import data from './data.json';
+// console.log(data);
 // 2. Name/Declare the class component.
 class Main extends React.Component {
     // Define the class with a render method. The render method should return something.
     render() {
+        // console.log(this.props.data);
+        let animal = [];
+        this.props.data.forEach(beast => {
+            console.log(beast);
+          animal.push(
+            <HornedBeast
+                title={beast.title}
+                imageUrl={beast.image_url}
+                description={beast.description}
+                horns={beast.horns}
+                keyword={beast.keyword}
+            />
+          )  
+        });
         return (
             <main>
-                <title>Gallery of Horns</title>
+                {/* <title>Gallery of Horns</title>
                 <HornedBeast 
                 title="Sparky" 
                 imageUrl="./beardedDragon.jpeg" 
@@ -22,7 +36,8 @@ class Main extends React.Component {
                 title="Buffy"
                 imageUrl="./blackRhino.jpeg"
                 description="Buffy is big and majestic."
-                />
+                /> */}
+                {animal}
             </main>
         );
     }
