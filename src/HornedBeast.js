053a0cda96a7d1287a4ca2f9fe658fb9fce966a6
Katrina Lenell Hill <khill365@gmail.com>
2022-03-15 22:59:26 -0700

    commented out import Button

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 
 import './HornedBeast.css';
 
