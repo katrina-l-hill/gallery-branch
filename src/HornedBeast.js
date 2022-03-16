04e039392a7695af6b3da600d69124b12bb1c6fe
Katrina Lenell Hill <khill365@gmail.com>
2022-03-15 20:22:54 -0700

    made img tag into separate rows

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -12,7 +12,11 @@ class HornedBeast extends React.Component {
         <title>Gallery of Horns</title>
           <h2>{this.props.title}</h2>
           <p>{this.props.description}</p>
-          <img src={this.props.imageUrl} alt={this.props.title} title={this.props.description}></img>
+          <img 
+            src={this.props.imageUrl} 
+            alt={this.props.title} 
+            title={this.props.description}
+            />
         </article>
       )
     }
