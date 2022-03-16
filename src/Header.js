6144229add164c9e353b6760708b05a55fc18cbc
Katrina Lenell Hill <khill365@gmail.com>
2022-03-15 21:37:49 -0700

    imported Header css

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,8 @@
 // 1. Start by creating a class component. Always start by importing React.
 import React from 'react';
 
+import './Header.css';
+
 // 2. Name/Declare the class component.
 class Header extends React.Component {
     // Define the class with a render method. The render method should return something.
