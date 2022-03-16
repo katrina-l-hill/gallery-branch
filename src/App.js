b708659556da11066c0257e852705e54559e13dd
Katrina Lenell Hill <khill365@gmail.com>
2022-03-15 20:22:16 -0700

    added data for json file under Main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+// There are 3 basic parts to a React component.
+// 1. Start by creating a class component. Always start by importing React.
+import React from 'react';
+// need to import the Header from the Header.js file.
+import Header from './Header.js';
+// need to import the Main from the Main.js file.
+import Main from './Main.js';
+// / need to import the Footer from the Footer.js file.
+import Footer from './Footer.js';
+// need to add back css file that was deleted when initially deleted everything in this file.
 import './App.css';
+import data from './data.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 2. Name/Declare the class component.
+class App extends React.Component {
+  // Define the class with a render method. The render method should return something.
+  render() {
+    return (
+      <>
+        <Header />
+        <Main 
+          data={data}
+        />
+        <Footer />
+      </>
+    );
+  }
 }
 
+// 3. Export the class.
 export default App;
