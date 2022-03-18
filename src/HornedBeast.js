459586f2173d06dc9e7809df131f5023051cf22b
Katrina Lenell Hill <khill365@gmail.com>
2022-03-17 18:27:01 -0700

    cleaned up code

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -31,22 +31,17 @@ class HornedBeast extends React.Component {
     render() {
         return (
             <article>
-                {/* <title>Gallery of Horns</title> */}
                 <h2 onClick={this.props.openModal}>{this.props.title}</h2>
-                {/* <p>{this.props.description}</p> */}
                 <img onClick={this.handleImageClick}
                     src={this.props.imageUrl}
                     alt={this.props.title}
                     title={this.props.description}
                     show={this.state.showModal}
                 />
-                {/* <Button className="article-button">Button 1</Button>
-                <Button className="article-button">Button 2</Button> */}
                 <p>Click on {this.props.title} if it's your favorite</p>
                 <Card border="primary" style={{ width: '5rem' }}>
                     <Card.Body>
                         <p>{ }💚{this.state.favorited}</p>
-                        {/* <p>number of clicks: {this.state.favorited}</p> */}
                     </Card.Body>
                 </Card>
             </article>
